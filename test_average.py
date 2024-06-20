438e5d1592b7c4bd1971154e42c658f7c3319260
Leny Rousseau <leny.rousseau@icloud.com>
2024-06-20 11:15:36 +0200

    feat: show result

--- test_average.py
+++ test_average.py
@@ -5,15 +5,15 @@ def test_average():
 
     input1 = [1, 1, 1]
     result = average(input1)
-
     assert result == 1
+    print(result(1))
 
     input2 = [1, 2, 3]
     result = average(input2)
-
     assert result == 2
+    print(result(2))
 
     input2 = [10, 100, 1000]
     result = average(input2)
-
     assert result == 370
+    print(result(370))
