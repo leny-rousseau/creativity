6ebbc8d062a20e52bccc584c6563596fde838db3
Leny Rousseau <leny.rousseau@icloud.com>
2024-06-20 11:25:16 +0200

    fix: error

--- test_average.py
+++ test_average.py
@@ -6,14 +6,11 @@ def test_average():
     input1 = [1, 1, 1]
     result = average(input1)
     assert result == 1
-    print(f"Average of {input1} is {result}")
 
     input2 = [1, 2, 3]
     result = average(input2)
     assert result == 2
-    print(f"Average of {input2} is {result}")
 
     input3 = [10, 100, 1000]
     result = average(input3)
-    print(f"Average of {input3} is {result}")
-    print(result(370))
+    assert result == 370
